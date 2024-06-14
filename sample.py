5960fc830abd5ab269fe4178d42ddac5223510ab
unknown <ibrahimeenat24@gmail.com>
2024-06-14 09:30:13 +0100

    add

---
+++ sample.py
@@ -0,0 +1,12 @@
+for i in range(10):
+    print(i*38)
+
+color = ['white', 'black']
+for clr in color:
+        print (clr[2])
+
+icolor = ['white', 'black']
+jcolor = ['green', 'blue']
+
+for clr in icolor:
+        print (icolor, jcolor)
